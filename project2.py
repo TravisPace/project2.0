56fc296f87e69855a9b1e95af2ece8d9801ee4e1
TravisPace <40004711+TravisPace@users.noreply.github.com>
2018-07-03 21:02:16 -0500

    Update project2.py

--- project2.py
+++ project2.py
@@ -1,6 +1,9 @@
 def dot(vector01,vector02):
   '''
-  This function calculates the dot product of the two vectors. This algorithim makes sure the lengths are equal to each other and if they do not equal each other, then the function does not run the code. This takes the lengths of vector01 and vector02 and multiplies each element together and creates a new list. This then takes the list and each element in the list and adds them together and comes up with a single integer.
+  This function calculates the dot product of the two vectors. This algorithim makes sure the lengths are equal to each other 
+  and if they do not equal each other, then the function does not run the code. This takes the lengths of vector01 and vector02
+  and multiplies each element together and creates a new list. This then takes the list and each element in the list and adds 
+  them together and comes up with a single integer.
   '''
   C = [ ]
   #C is an empty list.
@@ -28,7 +31,9 @@ def dot(vector01,vector02):
 
 def vecSubtraction(vector01,vector02):
   '''
-  This function adds to vectors together. This algorithim makes sure the lengths of the vectors are equal to each other. If the vector lengths are not equal to each other, then this function does not compute the outcome. This takes the vectors and subtract each element from the list and creates a single vector.
+  This function adds to vectors together. This algorithim makes sure the lengths of the vectors are equal to each other. 
+  If the vector lengths are not equal to each other, then this function does not compute the outcome. This takes the vectors 
+  and subtract each element from the list and creates a single vector.
   '''
   A = [ ]
   #A is an empty list.
@@ -50,8 +55,9 @@ def vecSubtraction(vector01,vector02):
 
 def scalarMultVec(scalar,vector):
   '''
-  This function takes a scalar and a vector and multiplies them together to create a single vector. If the scalar is equal to a list, the function does not run and computes and error. This takes the scalar and multiplies each element in the vector. This function 
-  then creates a new vector.
+  This function takes a scalar and a vector and multiplies them together to create a single vector. If the scalar is equal 
+  to a list, the function does not run and computes and error. This takes the scalar and multiplies each element in the vector.
+  This function then creates a new vector.
   '''
   if type(scalar) != int and type(scalar) != float and type(scalar) != complex:
     print('error')
@@ -71,8 +77,9 @@ def scalarMultVec(scalar,vector):
 
 def norm2(vector):
   '''
-  This function calculates the two norm. The result is the norm of the vector. This takes the absolute value of every element and squares them. This adds the result of the after each element is squared. Then, the function takes the square root of the result to return
-  the two norm.
+  This function calculates the two norm. The result is the norm of the vector. This takes the absolute value of every element 
+  and squares them. This adds the result of the after each element is squared. Then, the function takes the square root of the 
+  result to return the two norm.
   '''
   result = 0
   for i in range(len(vector)):
@@ -87,7 +94,10 @@ def norm2(vector):
 
 def modGramSchmidt(A):
   '''
-  This function calculates the modified Gram Schmidt process. This takes in the matrix A and computes q and r. This normalizes the matrix A to get q. Then, this does the dot product to get a scalar. Then, the scalar is multiplied with q columns and creates a new row of A. Then, this functions appends the list for q and takes each element of r that is calculated and put each r in a matrix.
+  This function calculates the modified Gram Schmidt process. This takes in the matrix A and computes q and r. 
+  This normalizes the matrix A to get q. Then, this does the dot product to get a scalar. Then, the scalar is multiplied 
+  with q columns and creates a new row of A. Then, this functions appends the list for q and takes each element of r that 
+  is calculated and put each r in a matrix.
   '''
   n = len(A)
   m = len(A[0])
@@ -111,16 +121,18 @@ def modGramSchmidt(A):
       B = scalarMultVec(r[j][i],q[i])
       #This is a temporary variable for the scalarMultVec function.
       A[j] = vecSubtraction(A[j],B)
-      #
+      #This takes each element and subtracts by the variable B
   return [q, r]
 
-A = [[1,1,1,1,1,1,1,1,1,1],[.55,.6,.65,.7,.75,.8,.85,.9,.95,1],[.3025,.36,.4225,.49,.5625,.64,.7225,.81,.9025,1],[.166375,.216,.274625,.343,.421875,.512,.614125,.729,.857375,1]]
+A = [[1,1,1,1,1,1,1,1,1,1],[.55,.6,.65,.7,.75,.8,.85,.9,.95,1],[.3025,.36,.4225,.49,.5625,.64,.7225,.81,.9025,1],
+     [.166375,.216,.274625,.343,.421875,.512,.614125,.729,.857375,1]]
 
 qr = modGramSchmidt(A)
 
 def transposeMatVec(q,y):
   '''
-  This function takes the transpose of the matrix q and by the vector y. This takes the matrix q and switches the rows and columns. Then, this takes the matrix q and multiples by vector y to get a new vector C.
+  This function takes the transpose of the matrix q and by the vector y. This takes the matrix q and switches the rows and 
+  columns. Then, this takes the matrix q and multiples by vector y to get a new vector C.
   '''
   C = []
   #This is the empty set C that is being solved for
@@ -129,7 +141,7 @@ def transposeMatVec(q,y):
       if type(q[i][j]) != int and type(q[i][j]) != float and type(q[i][j]) != complex:
         print('error')
         return None
-  #This iterates the block executing the length of the matrix.
+        #This iterates the block executing the length of the matrix.
     total = 0
     for j in range(len(y)):
       if type(y[j]) != int and type(y[j]) != float and type(y[j]) != complex:
@@ -143,21 +155,34 @@ def transposeMatVec(q,y):
     #This adds the product together.
   return C
   #This is returning the data to make the vector C
+  
 y = [1.102,1.099,1.017,1.111,1.117,1.152,1.265,1.380,1.575,1.857]
 C = transposeMatVec(qr[0],y)
 
 def backSub(R,b):
   '''
-  This takes the matrix r and the answer C to solve for x. This takes the reverse itiration of r. For j, this starts at the element i + 1 and goes to length C. This takes the element in C and subtracts the product of the matrix times the element in C. Then, divide the answer by the diagonal of r. This then returns the vector x that solves the equation Rx=b.
+  This takes the matrix r and the answer C to solve for x. This takes the reverse itiration of r. For j, this starts at  the element i + 1 and goes to length C. This takes the element in C and subtracts the product of the matrix times the  element in C. Then, divide the answer by the diagonal of r. This then returns the vector x that solves the equation Rx=b.
   '''
   k = len(b) - 1
   C = [0] * len(b)
+  #This creates a zero vector.
   C[k] = b[k] / R[k][k]
+  #Defining the function, taking value from b and dividing by the diagonal of R.
   for i in reversed(range(len(b))):
-    C[i] = b[i]
     for j in range(i + 1,len(b)):
+      if type(R[i][j]) != int and type(R[i][j]) != float and type(R[i][j]) != complex:
+        print('error')
+        return None
+    C[i] = b[i]
+    #Defining the element of C as the element of B.
+    for j in range(i + 1,len(b)):
+      if type(b[j]) != int and type(b[j]) != float and type(b[j]) != complex:
+        print('error')
+        return None
       C[i] = C[i] - C[j] * R[j][i]
+      #multiplying the matrix and vector together and subtracting that from original C.
     C[i] = C[i] / R[i][i]
+    #Dividing the result by the diagonal.
   return C
 
 R = qr[1]
@@ -170,5 +195,3 @@ y = [1.102,1.099,1.017,1.111,1.117,1.152,1.265,1.380,1.575,1.857]
 qr = modGramSchmidt(A) = (q,r)
 b = transposeMatVec(qr[0],y)
 c = backSub(qr[1],b)
-
-print(c[0],c[1] * x,c[2] * x^2,c[3] * x^3)
